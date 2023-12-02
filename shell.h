aa15c0594b90d850ee8b62ff9545aa197c56e74a
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2023-12-02 12:05:11 +0100

    Ala barakati lah... all in one header created

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+
+
+
+
+
+#endif
